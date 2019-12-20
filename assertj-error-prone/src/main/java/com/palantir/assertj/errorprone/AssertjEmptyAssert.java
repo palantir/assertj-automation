38acb51e9074da7ab00ada82939ceef61b92b8d7
Michael Wintermeyer <michaelw@palantir.com>
2019-12-20 12:37:14 -0500

    Add isEmpty error prone check

---
+++ assertj-error-prone/src/main/java/com/palantir/assertj/errorprone/AssertjEmptyAssert.java
@@ -0,0 +1,75 @@
+/*
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.assertj.errorprone;
+
+import com.google.auto.service.AutoService;
+import com.google.common.collect.Iterables;
+import com.google.errorprone.VisitorState;
+import com.google.errorprone.fixes.SuggestedFix;
+import com.google.errorprone.fixes.SuggestedFixes;
+import com.google.errorprone.matchers.ChildMultiMatcher;
+import com.google.errorprone.matchers.Matcher;
+import com.google.errorprone.matchers.Matchers;
+import com.google.errorprone.matchers.method.MethodMatchers;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodInvocationTree;
+import java.util.List;
+import java.util.Optional;
+
+@AutoService(AssertjChecker.class)
+public final class AssertjEmptyAssert implements AssertjChecker {
+
+    private static final String DESCRIPTION =
+            "Prefer using AssertJ isEmpty/isNotEmpty matchers instead of equality checks with a constant \"\".";
+
+    private static final Matcher<ExpressionTree> stringEqualMatcher = MethodMatchers.instanceMethod()
+            .onDescendantOf("org.assertj.core.api.AbstractStringAssert")
+            .named("isEqualTo");
+
+    private static final Matcher<ExpressionTree> stringNotEqualMatcher = MethodMatchers.instanceMethod()
+            .onDescendantOf("org.assertj.core.api.AbstractStringAssert")
+            .named("isNotEqualTo");
+
+    private static final Matcher<ExpressionTree> empty =
+            Matchers.ignoreParens(Matchers.stringLiteral(""));
+
+    private static final Matcher<ExpressionTree> matcher = Matchers.methodInvocation(
+            Matchers.anyOf(stringEqualMatcher, stringNotEqualMatcher),
+            ChildMultiMatcher.MatchType.LAST,
+            Matchers.anyOf(empty));
+
+    @Override
+    public Optional<AssertjCheckerResult> matchMethodInvocation(MethodInvocationTree tree, VisitorState state) {
+        if (!matcher.matches(tree, state)) {
+            return Optional.empty();
+        }
+        List<? extends ExpressionTree> arguments = tree.getArguments();
+        if (arguments.size() != 1) {
+            return Optional.empty();
+        }
+        ExpressionTree argument = Iterables.getOnlyElement(arguments);
+        boolean expectEmpty = stringEqualMatcher.matches(tree, state);
+        return Optional.of(AssertjCheckerResult.builder()
+                .description(DESCRIPTION)
+                .fix(SuggestedFix.builder()
+                        .merge(SuggestedFixes.renameMethodInvocation(tree, expectEmpty ? "isEmpty" : "isNotEmpty",
+                                state))
+                        .replace(argument, "")
+                        .build())
+                .build());
+    }
+}
